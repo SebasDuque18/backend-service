e6ed9259b2b381b2e50e1424ce4b9fd4c89cdfe3
guzmansandra <guzmanriverasandramilena@gamil.com>
2024-11-01 12:44:16 -0500

    Ajustes en el config del server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import express { application } from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import { AppDataSource } from "./data-source";
+import router from "./routers/product.Routes";
+import swaggerSpec from "./swagger/swagger";
 import swaggerUi from "swagger-ui-express";
-import { Application } from "express";
+
 
 const app: Application = express();
 const PORT = process.env.PORT ?? 3000;
@@ -12,3 +14,21 @@ app.use(cors());
 app.use(express.json());
 
 //rutas
+app.use("/API/", router); //ruta de productos
+
+//documentacion swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//inicializcion de la base de datos y el servido
+AppDataSource.initialize()
+.then(() => {
+    app.listen(() =>{
+        console.log(`Servidor corriendo en htpp://localhost:${PORT}\n`)
+        console.log(`Endpoints:`)
+        console.log(`API Products htpp://localhost:${PORT}/api/products`)
+        console.log(`Documentacio:`)
+        console.log(`Swagger en htpp://localhost:${PORT}/api-docs`);
+        
+    });
+})
+.catch((error) => console.log (error));
